b90871e30e12efdfee1f46227f86d68839bce87f
Purrincipal <symphonia108@gmail.com>
2026-03-05 13:10:36 -0700

    clock page edits

---
+++ test/JS.js
@@ -0,0 +1,13 @@
+const timelineItems = document.querySelectorAll(".timeline-item");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+});
+
+timelineItems.forEach(item => {
+  observer.observe(item);
+});
